5ddc41ec6d987b4d66d8eb7d7be69fffac54ac1b
Nguyễn Trung Hưng <43608238+dondi12345@users.noreply.github.com>
2024-07-21 17:37:51 +0700

    update

--- build/index.js
+++ build/index.js
@@ -7,8 +7,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
 const express_handlebars_1 = require("express-handlebars");
-const fs = require('fs');
-const handlebars = require('handlebars');
 const app = (0, express_1.default)();
 const port = 8000;
 //HTTP logger
@@ -20,10 +18,6 @@ app.engine('hbs', (0, express_handlebars_1.engine)({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path_1.default.join(__dirname, '../resources/views'));
-// Read the partial file
-const fish_io = fs.readFileSync(path_1.default.join(__dirname, '../FishIO/index.html'), 'utf8');
-// Register the partial
-handlebars.registerPartial('fish_io', fish_io);
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -41,9 +35,6 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 app.get('/test', (req, res) => {
-    res.render('partials/test');
-});
-app.get('/game/fish_io', (req, res) => {
     res.render('partials/test');
 });
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import express, { Request, Response, NextFunction } from 'express';
 import morgan  from 'morgan';
 import { engine } from 'express-handlebars';
 import sass from 'sass';
-const fs = require('fs');
-const handlebars = require('handlebars');
 
 const app = express()
 const port = 8000
@@ -25,12 +23,6 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '../resources/views'));
 
-// Read the partial file
-const fish_io = fs.readFileSync(path.join(__dirname, '../FishIO/index.html'), 'utf8');
-
-// Register the partial
-handlebars.registerPartial('fish_io', fish_io);
-
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -49,10 +41,6 @@ app.get('/contact', (req, res) => {
 });
 
 app.get('/test', (req, res)=>{
-  res.render('partials/test')
-})
-
-app.get('/game/fish_io', (req, res)=>{
   res.render('partials/test')
 })
 
